bf2062a228d5ab8e19090b7d98e93f52bba97b96
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-02-23 11:30:35 +0300

    work with LB1_B

---
+++ LB1_B/web_socket.js
@@ -0,0 +1,42 @@
+var textArea;
+var web_socket = null;
+
+var bstart;
+var bstop;
+window.onload = function() {
+    if(Modernizr.web_socket) {
+        WriteMessage("support", "Да");
+        textArea = document.getElementById("textArea");
+        bstart = document.getElementById("bstart");
+        bstop = document.getElementById("bstop");
+
+        bstart.disabled = false;
+        bstop.disabled = true;
+    }
+}
+
+function WriteMessage(idspan, txt) {
+    var span = document.getElementById(idspan);
+    span.innerHTML = txt;
+}
+
+function exe_start() {
+    if (web_socket == null) {
+        web_socket = new WebSocket("web_socket:localhost:55927/Websockets.websocket");
+        web_socket.onopen = function() { web_socket.send("Соеднение"); }
+        web_socket.onclose = function(s) { console.log("onsclose", s); }
+
+        web_socket.onmessage = function(evt) { textArea.innerHTML += "\n" + evt.data }
+
+        bstart.disabled = true;
+        bstop.disabled = false;
+    }
+}
+
+function exe_stop() {
+    web_socket.close(3001, "stopApplication");
+    web_socket = null;
+
+    bstart.disabled = false;
+    bstop.disabled = true;
+}
