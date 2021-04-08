1f68e2706b8de0fd57c828ffb302dbc80f75dc82
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-08 08:45:07 +0300

    Collection can be serialized to JSON in Add route

--- LB3/Controllers/DictController.cs
+++ LB3/Controllers/DictController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,11 +9,53 @@ using System.Web.Routing;
 
 namespace LB3.Controllers
 {
+    public class Contact
+    {
+        public static List<Contact> CONTACTS = new List<Contact>();
+        public Contact() 
+        {
+            CONTACTS.Add(new Contact("Helkar", "+1 800 987 353"));
+            CONTACTS.Add(new Contact("Bill", "+1 800 123 444"));
+        }
+        public Contact(string _lastname, string _phone) 
+        {
+            this.lastname = _lastname;
+            this.phone = _phone;
+        }
+        public string lastname { get; set; }
+        public string phone { get; set; }
+
+        public static void writeJSON(List<Contact> _contacts) 
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamWriter sw = new StreamWriter(@"D:\json.txt"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, _contacts);
+            }
+            Console.WriteLine("Contacts has been saved to D:\\json.txt");
+        }
+        public static List<Contact> readJSON() 
+        {
+            List<Contact> readContacts;
+
+            JsonSerializer deserializer = new JsonSerializer();
+
+            using (StreamReader sr = new StreamReader(@"D:\json.txt"))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                readContacts = (List<Contact>)deserializer.Deserialize(reader);
+            }
+            return readContacts;
+        }
+    }
     public class DictController : Controller
     {
         // GET: Dict
         public ActionResult Index()
         {
+            Contact createCollection = new Contact();
             return View();
         }
         public ActionResult Add() 
@@ -21,19 +65,32 @@ namespace LB3.Controllers
 
         public string AddSave() 
         {
-            /*string ip = requestContext.HttpContext.Request.UserHostAddress;
-            var response = requestContext.HttpContext.Response;
-            response.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");*/
-            //return "RETURN RESULT";
-
-            // Извлечь отправленные данные из Request.Form 
             string lastname = Request.Form["lastname"];
             string phone = Request.Form["phone"];
-            return "<h2>" + lastname + " --- " + phone + "</h2>";
+
+            Contact newContact = new Contact();
+            newContact.lastname = lastname;
+            newContact.phone = phone;
+
+            Contact.CONTACTS.Add(newContact);
+            Contact.writeJSON(Contact.CONTACTS);
+            return "<h2>Contact has been added - [" + lastname + " --- " + phone + "]</h2>";
         }
-        /*public ActionResult Update() { }
-        public ActionResult UpdateSave() { }
-        public ActionResult Delete() { }
-        public ActionResult DeleteSave() { }*/
+        /*public ActionResult Update() 
+        {
+            return View();
+        }
+        public ActionResult UpdateSave() 
+        { 
+
+        }
+        public ActionResult Delete() 
+        {
+            return View();
+        }
+        public ActionResult DeleteSave() 
+        {
+            
+        }*/
     }
 }
