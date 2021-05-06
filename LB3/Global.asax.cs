b912a004ce281c71ca565462d62e81e630ac93e4
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-06 09:03:51 +0300

    Now I can handle errors

--- LB3/Global.asax.cs
+++ LB3/Global.asax.cs
@@ -1,3 +1,4 @@
+using LB3.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,60 @@ namespace LB3
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            var httpContext = ((MvcApplication)sender).Context;
+            var currentController = string.Empty;
+            var currentAction = string.Empty;
+            var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+
+            if (currentRouteData != null)
+            {
+                if (!string.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+                {
+                    currentController = currentRouteData.Values["controller"].ToString();
+                }
+
+                if (!string.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+                {
+                    currentAction = currentRouteData.Values["action"].ToString();
+                }
+            }
+
+            var ex = Server.GetLastError();
+
+            var controller = new ErrorController();
+            var routeData = new RouteData();
+            var action = "NotFound";
+
+            if (ex is HttpException)
+            {
+                switch (((HttpException)ex).GetHttpCode())
+                {
+                    case 403:
+                        action = "AccessDenied";
+                        break;
+                    case 404:
+                        action = "NotFound";
+                        break;
+                    default:
+                        action = "HttpError";
+                        break;
+                }
+            }
+
+            httpContext.ClearError();
+            httpContext.Response.Clear();
+            httpContext.Response.StatusCode = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
+            httpContext.Response.TrySkipIisCustomErrors = true;
+
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = action;
+
+            controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+            ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
         }
     }
 }
