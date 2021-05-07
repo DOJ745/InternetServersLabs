7296068043e56ee35bc151ef8d0e5dc54e04e229
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-07 08:56:37 +0300

    Created db and added routes to config

--- LB4/App_Start/RouteConfig.cs
+++ LB4/App_Start/RouteConfig.cs
@@ -13,7 +13,17 @@ namespace LB4
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Dict", action = "Index", id = UrlParameter.Optional }
+            );
 
+            routes.MapRoute(
+                name: "Error",
+                url: "{*url}",
+                defaults: new { controller = "Error", action = "NotFound" }
+            );
 
             routes.MapRoute(
                 name: "Default",
