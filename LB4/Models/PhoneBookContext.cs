0674252e2d6d029d14652fdfc32b9c985adce7c8
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-07 08:51:41 +0300

    Created model for contact

---
+++ LB4/Models/PhoneBookContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace LB4.Models
+{
+    public class PhoneBookContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<PhoneBookContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
