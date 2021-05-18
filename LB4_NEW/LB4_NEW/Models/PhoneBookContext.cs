f78a813fed33b2b05711a3f0fa12ca61884aa07d
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-18 09:18:19 +0300

    Added database an EntityFramework

---
+++ LB4_NEW/LB4_NEW/Models/PhoneBookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LB4_NEW.Models
+{
+    public class PhoneBookContext: DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<PhoneBookContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
