dff03c62a681dbc41f1ab6c32719a9039f7e5e49
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-18 09:59:27 +0300

    Adding element works fine

--- LB4_NEW/LB4_NEW/Controllers/DictController.cs
+++ LB4_NEW/LB4_NEW/Controllers/DictController.cs
@@ -1,17 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LB4_NEW.Models;
+using System;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace LB4_NEW.Controllers
 {
     public class DictController : Controller
     {
+        PhoneBookContext DB = new PhoneBookContext();
+
+
         // GET: Dict
         public ActionResult Index()
         {
+            ViewBag.PhoneBooks = DB.Contacts.OrderBy(entry => entry.LastName);
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult AddSave(string lastName, string phone)
+        {
+            DB.Contacts.Add(new Contact() { LastName = lastName, Number = phone });
+            DB.SaveChanges();
+
+            ViewBag.PhoneBooks = DB.Contacts;
+            return Redirect("/Dict/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(string id)
+        {
+            ViewBag.Id = id;
+            ViewBag.PhoneBooks = DB.Contacts;
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult UpdateSave(string id, string lastName, string phone)
+        {
+            Contact contact = DB.Contacts.Find(Int32.Parse(id));
+            if (contact != null)
+            {
+                contact.LastName = lastName;
+                contact.Number = phone;
+
+                DB.Entry(contact).State = EntityState.Modified;
+                DB.SaveChanges();
+
+                ViewBag.PhoneBooks = DB.Contacts;
+            }
+
+            ViewBag.PhoneBooks = DB.Contacts;
+            return Redirect("/Dict/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteSave(string id)
+        {
+            Contact contact = DB.Contacts.Find(Int32.Parse(id));
+            if (contact != null)
+            {
+                DB.Contacts.Remove(contact);
+                DB.SaveChanges();
+
+                ViewBag.PhoneBooks = DB.Contacts;
+            }
+            return View("Index");
         }
     }
 }
--- LB4_NEW/LB4_NEW/Models/Contact.cs
+++ LB4_NEW/LB4_NEW/Models/Contact.cs
@@ -8,7 +8,7 @@ namespace LB4_NEW.Models
     public class Contact
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string LastName { get; set; }
         public string Number { get; set; }
     }
 }
