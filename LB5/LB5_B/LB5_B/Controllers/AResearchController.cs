e024179d0fc8a8e7b55ac3134a8eb03220fff834
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-31 18:08:40 +0300

    AResearch and filters works fine

--- LB5/LB5_B/LB5_B/Controllers/AResearchController.cs
+++ LB5/LB5_B/LB5_B/Controllers/AResearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LB5_B.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,26 @@ namespace LB5_B.Controllers
         {
             return View();
         }
+
+        [ActionFilter]
+        public ActionResult AA(int? id)
+        {
+            Response.Write("<div class=\"container\"><h2>AA Action(ID): " + id.Value + "</h2></div>");
+            return View("Index");
+        }
+
+        [ExceptionFilter]
+        public ActionResult AE()
+        {
+            throw new Exception("Exception message");
+        }
+
+        [ResultFilter]
+        public ActionResult AR()
+        {
+            Response.Write("<div class=\"container\"><h2>AR Action</h2></div>");
+            return View("Index");
+        }
+
     }
 }
