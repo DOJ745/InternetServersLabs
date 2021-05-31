0cd5a879d4c1750126b44a69793df0c49cc0ae93
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-05-31 18:38:40 +0300

    CHResearch works

--- LB5/LB5_B/LB5_B/Controllers/CHResearchController.cs
+++ LB5/LB5_B/LB5_B/Controllers/CHResearchController.cs
@@ -8,10 +8,28 @@ namespace LB5_B.Controllers
 {
     public class CHResearchController : Controller
     {
+        private static int counter = 0;
+
         // GET: CHResearch
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 5)]
+        public ActionResult AD()
+        {
+            Response.Write("<h3>Counter: " + (++counter) + "</h3>");
+            return View("Index");
+        }
+
+        [HttpPost]
+        [OutputCache(Duration = 7)]
+        public ActionResult AP(int x, int y)
+        {
+            Response.Write("<h3>Counter: " + DateTime.Now.ToString() + " --- Result --- " + (x + y) + "</h3>");
+            return View("Index");
         }
     }
 }
