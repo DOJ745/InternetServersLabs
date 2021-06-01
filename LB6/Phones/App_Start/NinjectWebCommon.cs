b1a70d8b4937455970a6d8a9f97ada97af1ef0b6
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-06-01 21:48:43 +0300

    Cosmetic changes again

--- LB6/Phones/App_Start/NinjectWebCommon.cs
+++ LB6/Phones/App_Start/NinjectWebCommon.cs
@@ -45,6 +45,7 @@ namespace Phones.App_Start
             {
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
                 RegisterServices(kernel);
                 return kernel;
             }
