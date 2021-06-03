cd51da33bef7fa715d467d3e71c38c99a3eb9e62
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-06-03 18:33:41 +0300

    Added another variant of LB6

---
+++ LB6_VER2/lab03/Helpers/AditionalHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab03.Helpers
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString AditionalHelp(this HtmlHelper html)
+        {
+            TagBuilder tag = new TagBuilder("form");
+            tag.GenerateId("some");
+            tag.Attributes.Add("action", "/dict/addsave");
+            tag.Attributes.Add("method", "post");
+
+            TagBuilder itemTag = new TagBuilder("input");
+            itemTag.Attributes.Add("type", "text");
+            itemTag.Attributes.Add("name", "surname");
+            itemTag.Attributes.Add("placeholder", "surname");
+
+            TagBuilder itemTag1 = new TagBuilder("input");
+            itemTag1.Attributes.Add("type", "number");
+            itemTag1.Attributes.Add("name", "telephone");
+            itemTag1.Attributes.Add("placeholder", "telephone");
+
+            TagBuilder itemTag2 = new TagBuilder("input");
+            itemTag2.Attributes.Add("type", "submit");
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
